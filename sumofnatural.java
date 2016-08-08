32a0bb8ad03e82669900cb2a6d53b7fee269952c
thilakas <thilikas29@gmail.com>
2016-08-08 10:27:42 +0530

    Add files via upload

---
+++ sumofnatural.java
@@ -0,0 +1,19 @@
+package logic;
+
+import java.util.Scanner;
+
+public class sumofnatural {
+	     public static void main(String[] args)
+	     {
+	        int n,i=1,sum=0;
+	        Scanner input=new Scanner(System.in);
+	        System.out.print("Enter Number :");
+	        n=input.nextInt();
+	        do
+	            {
+	                 sum=sum+i;
+	                 i +=1;
+	            }   while(i<=n);
+	                 System.out.print("Sum of First " + n + " Numbers = "+sum);
+	     } 
+}
